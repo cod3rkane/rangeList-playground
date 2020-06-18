2ecadaa2159545c9ddfd650e0b779fe87c9c2458
Cod3rkane <jcesar.developer@gmail.com>
2020-06-18 16:58:57 -0400

    RangeList.js

---
+++ src/RangeList.js
@@ -0,0 +1,23 @@
+class RangeList {
+    /**
+    * Adds a range to the list
+    * @param {Array<number>} range - Array of two integers that specify
+    beginning and end of range.
+    */
+    add(range) {
+        // TODO: implement this
+    } /**
+    * Removes a range from the list
+    * @param {Array<number>} range - Array of two integers that specify
+    beginning and end of range.
+    */
+    remove(range) {
+        // TODO: implement this
+    }
+    /**
+     * Prints out the list of ranges in the range list
+     */
+    print() {
+        // TODO: implement this
+    }
+}
