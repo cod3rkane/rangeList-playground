b5063b55f331aaa39eb4959b703ee6eea7c58fe3
Cod3rkane <jcesar.developer@gmail.com>
2020-06-18 18:30:34 -0400

    adding all test cases

--- test/RangeList.test.js
+++ test/RangeList.test.js
@@ -15,3 +15,57 @@ test('Should display: [1, 5)', () => {
   rl.print();
   expect(output).toBe('[1, 5)');
 });
+
+test('Should display: [1, 5) [10, 20)', () => {
+  rl.add([10, 20]);
+  rl.print();
+  expect(output).toBe('[1, 5) [10, 20)');
+});
+
+test('Should display: [1, 5) [10, 20)', () => {
+  rl.add([20, 20]);
+  rl.print();
+  expect(output).toBe('[1, 5) [10, 20)');
+});
+
+test('Should display: [1, 5) [10, 21)', () => {
+  rl.add([20, 21]);
+  rl.print();
+  expect(output).toBe('[1, 5) [10, 21)');
+});
+
+test('Should display: [1, 5) [10, 21)', () => {
+  rl.add([2, 4]);
+  rl.print();
+  expect(output).toBe('[1, 5) [10, 21)');
+});
+
+test('Should display: [1, 8) [10, 21)', () => {
+  rl.add([3, 8]);
+  rl.print();
+  expect(output).toBe('[1, 8) [10, 21)');
+});
+
+test('Should display: [1, 8) [10, 21)', () => {
+  rl.remove([10, 10]);
+  rl.print();
+  expect(output).toBe('[1, 8) [10, 21)');
+});
+
+test('Should display: [1, 8) [11, 21)', () => {
+  rl.remove([10, 11]);
+  rl.print();
+  expect(output).toBe('[1, 8) [11, 21)');
+});
+
+test('Should display: [1, 8) [11, 15) [17, 21)', () => {
+  rl.remove([15, 17]);
+  rl.print();
+  expect(output).toBe('[1, 8) [11, 15) [17, 21)');
+});
+
+test('Should display: [1, 3) [19, 21)', () => {
+  rl.remove([3, 19]);
+  rl.print();
+  expect(output).toBe('[1, 3) [19, 21)');
+});
