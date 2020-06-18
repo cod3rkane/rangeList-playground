984cb891c9c8e536ab8c3b9a48d4f24be2002282
Cod3rkane <jcesar.developer@gmail.com>
2020-06-18 18:23:31 -0400

    improves test

--- test/RangeList.test.js
+++ test/RangeList.test.js
@@ -2,8 +2,16 @@ import RangeList from '../src/RangeList';
 
 const rl = new RangeList();
 
-test('first case test', () => {
+let output = '';
+const loggerCatch = (input) => {
+  output = input;
+
+  return true;
+};
+console.log = jest.fn(loggerCatch);
+
+test('Should display: [1, 5)', () => {
   rl.add([1, 5]);
-  expect(rl.print()).toBe('[1, 5)');
-  // Should display: [1, 5)
+  rl.print();
+  expect(output).toBe('[1, 5)');
 });
