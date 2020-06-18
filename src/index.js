b22ba873fa4b06c3dffaf0355d46d7fa0e82ebd0
Cod3rkane <jcesar.developer@gmail.com>
2020-06-18 19:02:05 -0400

    index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-const test = [1, 2, 3, 4];
+import RangeList from './RangeList';
 
-console.log(test.values());
+const rl = new RangeList();
+rl.add([1, 5]);
+rl.print();
+// Should display: [1, 5)
+
+rl.add([10, 20]);
+rl.print();
+// Should display: [1, 5) [10, 20)
+
+rl.add([20, 20]);
+rl.print();
+// Should display: [1, 5) [10, 20)
+
+rl.add([20, 21]);
+rl.print();
+// Should display: [1, 5) [10, 21)
+
+rl.add([2, 4]);
+rl.print();
+// Should display: [1, 5) [10, 21)
+
+rl.add([3, 8]);
+rl.print();
+// Should display: [1, 8) [10, 21)rl.remove([10, 10]);
+
+rl.print();
+// Should display: [1, 8) [10, 21)
+
+rl.remove([10, 11]);
+rl.print();
+// Should display: [1, 8) [11, 21)
+
+rl.remove([15, 17]);
+rl.print();
+// Should display: [1, 8) [11, 15) [17, 21)
+
+rl.remove([3, 19]);
+rl.print();
+// Should display: [1, 3) [19, 21)
+
+console.log('\n\n');
+console.log('Hi, This test was solved by Cod3rKane - Julio Cesar');
+console.log('Reach me at: ');
+console.log('https://www.linkedin.com/in/cod3rkane/');
+console.log('https://github.com/cod3rkane');
